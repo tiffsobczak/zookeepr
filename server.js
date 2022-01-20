f74622644849cc605c615e4066a11770ab6c04d3
Tiffany <tiff.sobczak@gmail.com>
2022-01-20 17:04:00 -0600

    added 404 error

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const {animals} = require('./data/animals');
@@ -41,7 +43,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
-
+function findById(id, animalsArray) {
+    const result =animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
 
 app.get('/api/animals',(req, res) => {
     let results = animals;
@@ -49,6 +54,14 @@ app.get('/api/animals',(req, res) => {
         results= filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res)=> {
+    const result=findById(req.params.id, animals);
+    if (result) {
+    res.json(result);
+} else {
+    res.send(404);
+}
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
